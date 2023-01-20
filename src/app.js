1fe12bd3d367f297471e45ff23f8459af595e4e7
Jacob Nartey <jnartey@gmail.com>
2023-01-20 11:18:04 -0500

    Completed module

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Index'});
+});
+
+app.listen(PORT, () => {
+  console.log(`PS Project Running on port ${PORT}!`);
+});
