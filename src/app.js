de13651d7a41b44b1d3eac7830c211f81c47196e
Jacob Nartey <jnartey@gmail.com>
2023-01-20 11:23:59 -0500

    Completed module

--- src/app.js
+++ src/app.js
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+//Rendering static contents
+app.use(express.static(path.join(__dirname, '/public/')));
+
 app.get('/', (req, res) => {
   res.render('index', {title: 'Index'});
 });
